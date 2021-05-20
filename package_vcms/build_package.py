3ea9a273273ea12a7cda19543fd5c2d110e15920
wangfeng <0049003159@znv.com>
2021-05-20 09:10:29 +0800

    调整autocommit设置

--- package_vcms/build_package.py
+++ package_vcms/build_package.py
@@ -197,7 +197,7 @@ class BuildMysql(Build):
             copyfile(path.join(self.config.mysql_sql_script_base_dir,i),path.join(self.config.package_dir,path.basename(i)))
         #生成一个安装配置文件,这个给shell脚本使用
         with open(path.join(self.config.package_dir,'config.param'),'xt',encoding='utf8') as f:
-            for i in ('mysql_sync_ip=','mysql_sync_port=','mysql_user=','mysql_passwd=','mysql_software_base=','ignore_error=Y','mysql_socket='):
+            for i in ('mysql_sync_ip=','mysql_sync_port=8306','mysql_user=root','mysql_passwd=@znv_2014','mysql_software_base=','ignore_error=Y','mysql_socket='):
                 f.write(i+'\n')
             f.flush()
         #生成一个安装配置文件,这个给python脚本使用
